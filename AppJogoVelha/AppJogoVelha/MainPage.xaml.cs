0685be9bd16a017028cae5dcdd545a9bb2c4ac47
Usuario <Usuario@CPD02-M00>
2022-03-08 11:35:13 -0300

    Adição de Comportamos nos botões e verificação de quem ganhou (exemplo)

--- AppJogoVelha/AppJogoVelha/MainPage.xaml.cs
+++ AppJogoVelha/AppJogoVelha/MainPage.xaml.cs
@@ -17,12 +17,15 @@ namespace AppJogoVelha
             InitializeComponent();
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
+            // Identificando qual botão disparou o evento
             Button btn = (Button)sender;
-
+            
+            // Desabilitando o botão
             btn.IsEnabled = false;
 
+            // Trocando texto do botão de acordo com quem é a vez.
             if (vez == "X")
             {
                 btn.Text = "X";                
@@ -31,6 +34,22 @@ namespace AppJogoVelha
             {
                 btn.Text = "O";
                 vez = "X";
+            }
+
+            // Verificando se a linha 1 teve ganhador.
+            if(btn10.Text == "X" && btn11.Text == "X" && btn12.Text == "X")
+            {
+                await DisplayAlert("Parabéns", "O X ganhou!", "OK");
+                btn10.IsEnabled = true;
+                btn10.Text = "";
+                btn11.IsEnabled = true;
+                btn11.Text = "";
+                btn12.IsEnabled = true;
+                btn12.Text = "";
+
+            } else if (btn10.Text == "O" && btn11.Text == "O" && btn12.Text == "O")
+            {
+                DisplayAlert("Parabéns", "A O ganhou!", "OK");
             }
         }
     }
